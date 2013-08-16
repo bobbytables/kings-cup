36cb234612eb4a85b398c925368934dc03dd3fe3
Robert Ross <robert@creativequeries.com>
2013-08-15 20:20:10 -0700

    Shots shots shot shot shots shot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KingsCup::Application.config.secret_key_base = 'd71c0c0aba07131605f60662f650118f696dd2800f37625286c64065d89caffd0e8745ddb19edd4557e5b6383292e4ca48b8a45b3dbf6751851240a3b51748aa'
